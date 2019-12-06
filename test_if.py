bc1c593128130b030e23bd1e8a381e68b219831c
josed <diegoriosdrets@gmail.com>
2019-12-06 21:40:59 +0100

    if/else y formatos

---
+++ test_if.py
@@ -0,0 +1,8 @@
+numero_correcto = 17
+print("ADIVINA EL NUMERO")
+numero_adivinar = int(input("Dime un numero: "))
+
+if numero_adivinar == numero_correcto:
+    print("HAS GANADO")
+else:
+    print("HAS PERDIDO")
---
+++ test_max.py
@@ -0,0 +1,9 @@
+primer_numero = int(input("inserte el primer numero: "))
+segundo_numero = int(input("inserte el primer numero: "))
+tercer_numero = int(input("inserte el primer numero: "))
+cuarto_numero = int(input("inserte el primer numero: "))
+quinto_numero = int(input("inserte el primer numero: "))
+
+resultado = max(primer_numero,segundo_numero,tercer_numero,cuarto_numero,quinto_numero)
+
+print("El numero mas grande es: {}".format(resultado))
