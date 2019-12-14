986b4dbc882e8e9649d35e7d969b7d76635ce9bb
josed <diegoriosdrets@gmail.com>
2019-12-14 04:06:11 +0100

    Afinando un poco el combate_pokemon y creando una sencilla calculadora *necesario practicar mas con while*

---
+++ calculadora.py
@@ -0,0 +1,33 @@
+
+primer_numero = 0
+segundo_numero = 0
+repetir = 0
+
+## Preguntamos la operacion que quiere realizar ##
+
+operacion = input("¿Que operacion quieres realizar?(sumar/restar/multiplicar/dividir): ")
+
+## Pedimos los numeros con los que se va a realizar la operacion ##
+
+primer_numero = int(input("Primer numero: "))
+segundo_numero = int(input("Segundo numero: "))
+
+### Mostramos resultado ###
+
+
+if operacion == "sumar":
+        resultado_suma = float(primer_numero+segundo_numero)
+        print("Resultado: {}".format(resultado_suma))
+
+elif operacion == "restar":
+        resultado_resta = float(primer_numero-segundo_numero)
+        print("Resultado: {}".format(resultado_resta))
+
+elif operacion == "multiplicar":
+        resultado_multiplicacion = float(primer_numero * segundo_numero)
+        print("Resultado: {}".format(resultado_multiplicacion))
+
+elif operacion == "dividir":
+        resultado_division = float(primer_numero / segundo_numero)
+        print("Resultado: {}".format(resultado_division))
+
--- combate_pokemon.py
+++ combate_pokemon.py
@@ -13,32 +13,35 @@
 
 ##Estableciendo objetos##
 pokemon_elegido = input("¿Contra que pokemon quieres luchar? (Bulbasaur/Squirtle/Charmander)")
-vida_de_Pikachu = 100
-vida_de_enemigo = 0
+vida_Pikachu = 100
+vida_enemigo = 0
+ataque_enemigo = 0
 
 ##Eleccion de enemigo##
 if pokemon_elegido == "Squirtle":
-    vida_de_enemigo = 90
+    vida_enemigo = 90
+    ataque_enemigo = 8
 
-if pokemon_elegido == "Charmander":
-    vida_de_enemigo = 80
+elif pokemon_elegido == "Charmander":
+    vida_enemigo = 80
+    ataque_enemigo = 7
 
-if pokemon_elegido == "Bulbasaur":
-    vida_de_enemigo = 100
-
+elif pokemon_elegido == "Bulbasaur":
+    vida_enemigo = 100
+    ataque_enemigo = 12
 ## Bucle de ataques ##
 
-while vida_de_Pikachu > 0 and vida_de_enemigo > 0:
+while vida_Pikachu > 0 and vida_enemigo > 0:
 
     ###Ataques de Pikachu###
     ataque_elegido = input("¿Que ataque quieres elegir? (Chispazo / Bola voltio)")
     if ataque_elegido == "Chispazo":
-        vida_de_enemigo -= 10
+        vida_enemigo -= 10
         print("Tu enemigo ha sufrido 10 puntos de daño")
-    if ataque_elegido == "Bola voltio":
-        vida_de_enemigo -= 12
+    elif ataque_elegido == "Bola voltio":
+        vida_enemigo -= 12
         print("Tu enemigo ha sufrido 12 puntos de daño")
-    print("la vida de {} es de {} puntos".format(pokemon_elegido,vida_de_enemigo))
+    print("la vida de {} es de {} puntos".format(pokemon_elegido,vida_enemigo))
 
 ### terminar_turno = input("¿Has terminado tu turno?(Si/No)")
 ###  while terminar_turno == "Si":
@@ -49,23 +52,22 @@ while vida_de_Pikachu > 0 and vida_de_enemigo > 0:
         ### Ataques de los enemigos ###
 
     if pokemon_elegido == "Squirtle":
-        vida_de_Pikachu -= 8
-        print("Squirtle ataca provocandote unos 8 puntos de daño")
-    if pokemon_elegido == "Charmander":
-        vida_de_Pikachu -= 7
-        print("Charmander ataca provocandote unos 7 puntos de daño")
-    if pokemon_elegido == "Bulbasaur":
-        vida_de_Pikachu -= 12
-        print("Bulbasaur ataca provocandote unos 12 puntos de daño")
-    print("la vida de Pikachu es de {}".format(vida_de_Pikachu))
+        vida_Pikachu -= ataque_enemigo
+        print("{} ataca provocandote unos {} puntos de daño".format(pokemon_elegido,ataque_enemigo))
+    elif pokemon_elegido == "Charmander":
+        vida_Pikachu -= ataque_enemigo
+        print("{} ataca provocandote unos {} puntos de daño".format(pokemon_elegido,ataque_enemigo))
+    elif pokemon_elegido == "Bulbasaur":
+        vida_Pikachu -= ataque_enemigo
+        print("{} ataca provocandote unos {} puntos de daño".format(pokemon_elegido,ataque_enemigo))
+    print("la vida de Pikachu es de {}".format(vida_Pikachu))
 
 ##Mensaje de muerte de pokemon ##
 
-if vida_de_Pikachu <= 0:
+if vida_Pikachu <= 0:
     print("Pikachu ha sido derrotado")
 else:
     print("{} ha sido derrotado".format(pokemon_elegido))
 
 print("El combate ha terminado")
 
-#####MINUTO 16
