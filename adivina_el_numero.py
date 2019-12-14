16e75b375d7406d8911017751ef5f1747ae82729
josed <diegoriosdrets@gmail.com>
2019-12-14 04:38:09 +0100

    Juego de adivinar el numero muy mal hecho. pendiente de afinar

---
+++ adivina_el_numero.py
@@ -0,0 +1,12 @@
+##Adivina el numero##
+
+
+insertar_numero = input("Inserte numero a adivinar (que tu compañero no lo vea): ")
+
+adivina_numero = input("Adivina el numero secreto: ")
+
+if insertar_numero == adivina_numero:
+    print("HAS Ganado")
+
+else:
+    print("Has Perdido")
