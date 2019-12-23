f5f4bf7154015703baf496bdb9e2f7062cdc34f7
josed <diegoriosdrets@gmail.com>
2019-12-23 09:20:54 +0100

    Respasando ejercicios

---
+++ numero_more_peque.py
@@ -0,0 +1,12 @@
+numeros = []
+
+
+while len(numeros) < int(10):
+
+    while
+
+    numeros.append(int(input("introduce un numero: ")))
+    print("NUMERO AÑADIDO")
+
+
+print(numeros)
--- tabla_multiplicar.py
+++ tabla_multiplicar.py
@@ -1,11 +1,9 @@
 numero_elegido = int(input("Elige un numero: "))
-contador_while = 0
+contador = 11
 resultado = 0
-tabla = [int(5),int(6),int(7),int(8),int(9),int(10),int(11),int(12),int(13),int(14),int(15)]
 
-while contador_while < len(tabla):
-    for item in tabla:
-        contador_while += 1
-        if contador_while > 0:
-            resultado = item * numero_elegido
-            print("{} x {} = {}".format(item,numero_elegido,resultado))
+for item in range(1,11):
+        contador -= 1
+        resultado = contador * numero_elegido
+        if resultado == resultado:
+            print("{} x {} = {}".format(contador,numero_elegido,resultado))
